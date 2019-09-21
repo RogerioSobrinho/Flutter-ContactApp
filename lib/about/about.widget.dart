6c4442f32708050e477572b8671eafd9521c05f6
RogerioSobrinho <rogerioa.sobrinho@gmail.com>
2019-09-20 22:33:48 -0300

    created About Us page

---
+++ lib/about/about.widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("About Us")),
+      body: Container(),
+    );
+  }
+}
