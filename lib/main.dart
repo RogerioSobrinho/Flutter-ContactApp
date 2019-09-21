81b99021f3db6ba4743cc2d070e8accec978e3e0
RogerioSobrinho <rogerioa.sobrinho@gmail.com>
2019-09-20 22:31:10 -0300

    Initial Project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:contacts/home/home.widget.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
