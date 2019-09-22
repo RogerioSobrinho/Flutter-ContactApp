57d0d30891587de0cfb7c7ca3e084787808815ac
RogerioSobrinho <rogerioa.sobrinho@gmail.com>
2019-09-22 13:50:50 -0300

    created routes from pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
-import 'package:contacts/home/home.widget.dart';
 import 'package:flutter/material.dart';
+
+import 'pages/home/home.dart';
+import 'pages/create/create.dart';
+import 'pages/search/search.dart';
+import 'pages/about/about.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final routes = <String, WidgetBuilder>{
+    HomePage.tag: (context) => HomePage(),
+    AboutPage.tag: (context) => AboutPage(),
+    SearchPage.tag: (context) => SearchPage(),
+    CreatePage.tag: (context) => CreatePage(),
+  };
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
+      title: 'Flutter Contacts',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: HomePage(),
+      routes: routes,
     );
   }
 }
