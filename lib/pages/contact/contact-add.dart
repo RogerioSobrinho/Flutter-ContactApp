7bd893e98c7930dc221aaa48c7958d306350c739
RogerioSobrinho <rogerioa.sobrinho@gmail.com>
2019-09-24 01:20:10 -0300

    changed inputs from edit contact

--- lib/pages/contact/contact-add.dart
+++ lib/pages/contact/contact-add.dart
@@ -10,48 +10,63 @@ class ContactAddPage extends StatefulWidget {
 
 class _ContactAddPageState extends State<ContactAddPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _cFirstName = TextEditingController();
-  final TextEditingController _cLastName = TextEditingController();
+  final TextEditingController _cName = TextEditingController();
+  final TextEditingController _cNickName = TextEditingController();
+  final TextEditingController _cWork = TextEditingController();
   final TextEditingController _cPhoneNumber = TextEditingController();
   final TextEditingController _cEmail = TextEditingController();
-  final TextEditingController _cWork = TextEditingController();
+  final TextEditingController _cWebSite = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    final inputFirstName = TextFormField(
-      controller: _cFirstName,
+    final inputName = TextFormField(
+      controller: _cName,
       autofocus: true,
       keyboardType: TextInputType.text,
       decoration: InputDecoration(
-        labelText: 'First Name',
+        labelText: 'Nome',
+        hintText: 'Nome',
+        icon: Icon(Icons.person),
       ),
       validator: (value) {
         if (value.isEmpty) {
-          return 'Required';
+          return 'Obrigatório';
         }
         return null;
       },
     );
 
-    final inputLastName = TextFormField(
-      controller: _cLastName,
-      autofocus: false,
+    final inputNickName = TextFormField(
+      controller: _cNickName,
       keyboardType: TextInputType.text,
       decoration: InputDecoration(
-        labelText: 'Last Name',
+        labelText: 'Apelido',
+        hintText: 'Apelido',
+        icon: Icon(Icons.person),
+      ),
+    );
+
+    final inputWork = TextFormField(
+      controller: _cWork,
+      keyboardType: TextInputType.text,
+      decoration: InputDecoration(
+        labelText: 'Trabalho',
+        hintText: 'Trabalho',
+        icon: Icon(Icons.work),
       ),
     );
 
     final inputPhoneNumber = TextFormField(
       controller: _cPhoneNumber,
-      autofocus: false,
       keyboardType: TextInputType.phone,
       decoration: InputDecoration(
-        labelText: 'Phone Number',
+        labelText: 'Telefone',
+        hintText: 'Telefone',
+        icon: Icon(Icons.phone),
       ),
       validator: (value) {
         if (value.isEmpty) {
-          return 'Required';
+          return 'Obrigatório';
         }
         return null;
       },
@@ -59,71 +74,110 @@ class _ContactAddPageState extends State<ContactAddPage> {
 
     final inputEmail = TextFormField(
       controller: _cEmail,
-      autofocus: false,
       keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(
-        labelText: 'Email',
+        labelText: 'E-mail',
+        hintText: 'E-mail',
+        icon: Icon(Icons.email),
       ),
     );
 
-    final inputWork = TextFormField(
-      controller: _cWork,
-      autofocus: false,
+    final inputWebSite = TextFormField(
+      controller: _cWebSite,
       keyboardType: TextInputType.text,
       decoration: InputDecoration(
-        labelText: 'Work',
+        labelText: 'Site da Web',
+        hintText: 'Site da Web',
+        icon: Icon(Icons.web),
       ),
     );
 
-    Container content = Container(
-      child: Form(
-        key: _formKey,
-        child: ListView(
-          shrinkWrap: true,
-          padding: EdgeInsets.all(20),
+    final picture = Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          width: 120.0,
+          height: 120.0,
+          child: CircleAvatar(
+            child: Icon(
+              Icons.camera_alt,
+            ),
+          ),
+        ),
+      ],
+    );
+
+    Column content = Column(
+      children: <Widget>[
+        Row(
           children: <Widget>[
-            inputFirstName,
-            inputLastName,
-            inputPhoneNumber,
-            inputEmail,
-            inputWork,
+            Container(
+              padding: EdgeInsets.only(top: 30, left: 10),
+              child: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ),
           ],
         ),
-      ),
+        picture,
+        Expanded(
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              padding: EdgeInsets.all(20),
+              children: <Widget>[
+                inputName,
+                inputNickName,
+                inputWork,
+                inputPhoneNumber,
+                inputEmail,
+                inputWebSite,
+              ],
+            ),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(20),
+              child: RaisedButton(
+                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 80.0),
+                color: Colors.blue,
+                textColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(30.0),
+                ),
+                child: Text("Salvar"),
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    ModelContact contact = ModelContact();
+                    contact.insert({
+                      'name': _cName.text,
+                      'nickName': _cNickName.text,
+                      'work': _cWork.text,
+                      'phoneNumber': _cPhoneNumber.text,
+                      'email': _cEmail.text,
+                      'webSite': _cWebSite.text,
+                      'created': DateTime.now().toString()
+                    }).then((saved) {
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pushReplacementNamed(HomePage.tag);
+                    });
+                  }
+                },
+              ),
+            ),
+          ],
+        )
+      ],
     );
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.grey,
-        leading: IconButton(
-          icon: Icon(Icons.close),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        title: Text("Add contact"),
-        actions: <Widget>[
-          IconButton(
-            icon: Text("Save"),
-            onPressed: () {
-              if (_formKey.currentState.validate()) {
-                ModelContact contact = ModelContact();
-                contact.insert({
-                  'firstName': _cFirstName.text,
-                  'lastName': _cLastName.text,
-                  'phoneNumber': _cPhoneNumber.text,
-                  'email': _cEmail.text,
-                  'work': _cWork.text,
-                  'created': DateTime.now().toString()
-                }).then((saved) {
-                  Navigator.of(context).pop();
-                  Navigator.of(context).pushReplacementNamed(HomePage.tag);
-                });
-              }
-            },
-          )
-        ],
-      ),
       body: content,
     );
   }
